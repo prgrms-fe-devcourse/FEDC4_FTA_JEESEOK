8d4f32af920f1039a9c7e95ee85dc2bcc005910a
kimheeseok <kim1140129@gmail.com>
2023-09-07 21:11:19 +0900

    feat: #54 Like Api 구현

--- src/api/like/index.ts
+++ src/api/like/index.ts
@@ -1,8 +1,11 @@
-import { AxiosResponse } from 'axios';
 import request from '~/api';
 
-interface likeRequest {
+interface postLikeRequest {
   postId: string;
+}
+
+interface deleteLikeRequest {
+  likeId: string;
 }
 
 interface likeResponse {
@@ -13,8 +16,26 @@ interface likeResponse {
   updatedAt: string;
 }
 
-export const postLike = async (postId: likeRequest): Promise<likeResponse> => {
-  const result = await request.post<likeResponse>('/likes/create', postId);
+export const postLike = async (
+  postId: postLikeRequest
+): Promise<likeResponse | undefined> => {
+  try {
+    const result = await request.post<likeResponse>('/likes/create', postId);
+    return result.data;
+  } catch (e: unknown) {
+    return undefined;
+  }
+};
 
-  return result.data;
+export const deleteLike = async (
+  postId: deleteLikeRequest
+): Promise<likeResponse | undefined> => {
+  try {
+    const result = await request.delete<likeResponse>('/likes/delete', {
+      data: postId,
+    });
+    return result.data;
+  } catch (e: unknown) {
+    return undefined;
+  }
 };
