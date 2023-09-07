4a8e0bb34dd40d2eabcb52b92cd288abb6cd96fb
kimheeseok <kim1140129@gmail.com>
2023-09-07 15:20:56 +0900

    feat: #49 env변수명 수정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const request = axios.create({
-    baseURL: process.env.REACT_APP_API,
+    baseURL: process.env.REACT_APP_API_ENDPOINT,
     headers: {
         'Content-Type': 'application/json',
         Accept: '*/*',
