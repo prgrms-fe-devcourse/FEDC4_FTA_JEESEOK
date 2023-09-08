32958d803755398f31440abbd0bd2c1a57cba91c
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-09-08 15:13:32 +0900

    feat: #67 Response타입 변경, catch문 return 값 수정

--- src/api/search/index.ts
+++ src/api/search/index.ts
@@ -1,28 +1,51 @@
 import { Post, User } from '~/types';
 import request from '..';
 
+interface SearchUserResponse {
+  role: string;
+  emailVerified: boolean;
+  banned: boolean;
+  isOnline: boolean;
+  posts: [];
+  likes: [];
+  comments: [];
+  followers: [];
+  following: [];
+  notifications: [];
+  messages: [];
+  _id: string;
+  fullName: string;
+  email: string;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+  username: string;
+}
+
 export const searchUser = async (
   query: string
-): Promise<User[] | undefined> => {
+): Promise<SearchUserResponse[] | false> => {
   try {
-    const { data } = await request.get<User[] | undefined>(
+    const { data } = await request.get<SearchUserResponse[] | false>(
       `/search/users/${query}`
     );
     return data;
   } catch (error) {
     console.error(error);
+    return false;
   }
 };
 
 export const searchAll = async (
   query: string
-): Promise<(User | Post)[] | undefined> => {
+): Promise<(User | Post)[] | false> => {
   try {
-    const { data } = await request.get<(User | Post)[] | undefined>(
+    const { data } = await request.get<(User | Post)[] | false>(
       `/search/all/${query}`
     );
     return data;
   } catch (error) {
     console.error(error);
+    return false;
   }
 };
