404069dd760c1971c4e7cc2f7b552b21a1dbe782
kimheeseok <kim1140129@gmail.com>
2023-09-11 16:33:26 +0900

    feat: 헤더 컴포넌트 구현

---
+++ src/components/common/Header/headerStyle.ts
@@ -0,0 +1,32 @@
+import styled from '@emotion/styled';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  border-bottom: 1px solid black;
+  justify-content: space-between;
+`;
+
+export const LogoIcon = styled.img`
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`;
+
+export const BackIcon = styled.img`
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+`;
+
+type SearchIconProps = {
+  isHidden: boolean;
+};
+
+export const SearchIcon = styled.img<SearchIconProps>`
+  width: 40px;
+  height: 40px;
+  cursor: pointer;
+  visibility: ${({ isHidden }) => (isHidden ? 'visible' : 'hidden')};
+`;
