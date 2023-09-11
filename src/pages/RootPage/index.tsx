7f7de266ca50192f6062409496a09497d88582e3
seeyoujeong <chan3358@naver.com>
2023-09-12 02:49:30 +0900

    feat: RootPage 구현

--- src/pages/RootPage/index.tsx
+++ src/pages/RootPage/index.tsx
@@ -1,12 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
+import { getAuthorizationCheckApi } from '~/api/authorization';
+import { User } from '~/types';
+import Footer from './Footer';
+import Header from './Header';
 
 const RootPage = () => {
+  const [userId, setUserId] = useState('');
+
+  useEffect(() => {
+    const getUser = async () => {
+      const user = (await getAuthorizationCheckApi()) as unknown as
+        | User
+        | false;
+
+      if (user) setUserId(user._id);
+    };
+
+    getUser();
+  });
+
   return (
     <div>
-      <div>RootPage</div>
-      <div>Header</div>
+      <Header title="" isLogo={true} isSearch={true} />
       <Outlet />
-      <div>Footer</div>
+      <Footer userId={userId} />
     </div>
   );
 };
