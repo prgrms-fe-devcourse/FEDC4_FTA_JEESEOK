da6d7f7b92b0393ba01f81721afd09310a2325ef
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2023-09-13 02:40:36 +0900

    feat: #107 유저 정보 페이지 구현

---
+++ src/components/common/UserMainPage/style.ts
@@ -0,0 +1,25 @@
+import styled from '@emotion/styled';
+
+export const MainContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 15px;
+  height: 87vh;
+`;
+
+export const IntroduceArea = styled.div`
+  width: 70%;
+  height: 40%;
+  border: 2px solid #aed1ff;
+  border-radius: 16px;
+  padding: 30px 10px;
+  box-sizing: border-box;
+  background-color: #dfe7ff;
+  text-align: center;
+  font-size: 23px;
+  color: #c78eff;
+  white-space: normal;
+  word-wrap: break-word;
+`;
--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -1,9 +1,67 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { getUser } from '~/api/user';
+import UserPhoto from '~/assets/user.svg';
+import Image from '~/components/common/Image';
+import Tag from '~/components/common/Tag';
+import {
+  IntroduceArea,
+  MainContainer,
+} from '~/components/common/UserMainPage/style';
 
 const UserPage = () => {
   const { userId } = useParams();
+  const [userInfo, setUserInfo] = useState({
+    mbti: '',
+    introduce: '',
+  });
 
-  return <div>UserPage {userId}</div>;
+  useEffect(() => {
+    const getUserInfo = async () => {
+      if (userId) {
+        const data = await getUser(userId);
+
+        if (data && 'fullName' in data) {
+          const { fullName } = data;
+          const { mbti, introduce } = JSON.parse(fullName as string);
+
+          setUserInfo({ mbti, introduce });
+        }
+      }
+    };
+
+    getUserInfo();
+  }, [userId]);
+
+  const { mbti, introduce } = userInfo;
+
+  const tagProps = {
+    width: '80px',
+    height: '40px',
+    borderWidth: '0',
+    borderRadius: '12px',
+    backgroundColor: '#DFE7FF',
+    fontSize: '16px',
+    fontColor: '#c78eff',
+    borderColor: 'black',
+  };
+
+  return (
+    <MainContainer>
+      <Image
+        src={UserPhoto}
+        shape={'circle'}
+        width={200}
+        height={200}
+        mode={'cover'}
+        style={{ backgroundColor: '#DFE7FF' }}
+      />
+      <Tag {...tagProps} style={{ cursor: 'default' }}>
+        {mbti}
+      </Tag>
+      <IntroduceArea>{introduce}</IntroduceArea>
+    </MainContainer>
+  );
 };
 
 export default UserPage;
