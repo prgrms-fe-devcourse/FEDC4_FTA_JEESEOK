c44c776c22cd04099d515bceff3a13a889b53a91
seeyoujeong <chan3358@naver.com>
2023-09-13 03:29:36 +0900

    feat: #103 Footer 컴포넌트에 글쓰기 버튼 추가

--- src/pages/RootPage/Footer.tsx
+++ src/pages/RootPage/Footer.tsx
@@ -1,5 +1,60 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
+
+interface FooterProps {
+  userId: string | undefined;
+}
+
+const Footer = ({ userId }: FooterProps) => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const handleFooterClick = (page: string) => {
+    navigate(`/${page}`);
+  };
+
+  const handleCreateButtonClick = () => {
+    navigate('/post/create');
+  };
+
+  const contents = userId
+    ? [
+        { name: '게시판', page: 'post' },
+        { name: '알림', page: 'notification' },
+        { name: '마이', page: `user/${userId}` },
+      ]
+    : [
+        { name: '게시판', page: 'post' },
+        { name: '로그인', page: 'login' },
+      ];
+
+  return (
+    <FooterContainer>
+      {userId && ['/', '/post'].includes(pathname) && (
+        <CreateButton onClick={handleCreateButtonClick}>글쓰기</CreateButton>
+      )}
+      {contents.map(({ name, page }) => (
+        <FooterButton key={page} onClick={() => handleFooterClick(page)}>
+          {name}
+        </FooterButton>
+      ))}
+    </FooterContainer>
+  );
+};
+
+export default Footer;
+
+const CreateButton = styled.button`
+  position: absolute;
+  bottom: 80px;
+  right: 20px;
+  border-radius: 20px;
+  background-color: white;
+  width: 60px;
+  white-space: nowrap;
+  height: 30px;
+  cursor: pointer;
+`;
 
 export const FooterContainer = styled.div`
   max-width: 425px;
@@ -18,38 +73,3 @@ export const FooterButton = styled.button`
   background-color: white;
   cursor: pointer;
 `;
-
-interface FooterProps {
-  userId: string | undefined;
-}
-
-const Footer = ({ userId }: FooterProps) => {
-  const navigate = useNavigate();
-
-  const handleFooterClick = (page: string) => {
-    navigate(`/${page}`);
-  };
-
-  const contents = userId
-    ? [
-        { name: '게시판', page: 'post' },
-        { name: '알림', page: 'notification' },
-        { name: '마이', page: `user/${userId}` },
-      ]
-    : [
-        { name: '게시판', page: 'post' },
-        { name: '로그인', page: 'login' },
-      ];
-
-  return (
-    <FooterContainer>
-      {contents.map(({ name, page }) => (
-        <FooterButton key={page} onClick={() => handleFooterClick(page)}>
-          {name}
-        </FooterButton>
-      ))}
-    </FooterContainer>
-  );
-};
-
-export default Footer;
