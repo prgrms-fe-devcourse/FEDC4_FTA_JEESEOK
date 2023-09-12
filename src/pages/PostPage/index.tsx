9ec53403e051b91e34cf87d00c19bb6d37b6f1f4
seeyoujeong <chan3358@naver.com>
2023-09-12 16:08:03 +0900

    chore: PostPage index 코드 초기 상태로 되돌리기

--- src/pages/PostPage/index.tsx
+++ src/pages/PostPage/index.tsx
@@ -1,27 +1,9 @@
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
-import { getChannelPost } from '~/api/post';
-import { Post } from '~/types';
-
-const CHANEL_LOVE_ID = '64f57dd474128417c2689170';
-const CHANEL_RELATIONSHIP_ID = '64f96db08a4e9a3147d9117a';
-const CHANEL_JOB_ID = '64f57dc874128417c268916c';
-const CHANEL_MONEY_ID = '64f96d8e8a4e9a3147d91176';
+import { useParams } from 'react-router-dom';
 
 const PostPage = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [searchParams, setSearchParams] = useSearchParams();
+  const { postId } = useParams();
 
-  useEffect(() => {
-    const getCP = async () => {
-      const data = await getChannelPost(CHANEL_LOVE_ID, 0, 10);
-      console.log(data);
-    };
-
-    getCP();
-  });
-
-  return <div>PostPage</div>;
+  return <div>PostPage {postId}</div>;
 };
 
 export default PostPage;
