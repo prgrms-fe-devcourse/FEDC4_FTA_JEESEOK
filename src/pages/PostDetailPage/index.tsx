0eda562a5fa4861eac05f35d3b94b7cc0b114d50
kimheeseok <kim1140129@gmail.com>
2023-09-13 23:14:09 +0900

    feat: 게시글 디테일 페이지 구현

--- src/pages/PostDetailPage/index.tsx
+++ src/pages/PostDetailPage/index.tsx
@@ -1,9 +1,59 @@
-import { useParams } from 'react-router-dom';
+import { useEffect, useMemo, useState } from 'react';
+import styled from '@emotion/styled';
+import { readPost } from '~/api/post';
+import Header from '~/components/common/Header';
+import { Post } from '~/types';
+
+const PostDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const PostDetailBody = styled.div``;
+const Title = styled.p``;
+const Body = styled.p``;
 
 const PostDetailPage = () => {
-  const { postId } = useParams();
+  // const { postId } = useParams();
+  const [postDetailState, setPostDetailState] = useState<Post>();
+  const postId = '6500046076a8b6216821e042';
+  const postDetailApiCall = async () => {
+    const postDetailResult = (await readPost(postId)) as unknown as Post;
+    setPostDetailState(postDetailResult);
+    console.log(postDetailResult);
+  };
 
-  return <div>PostDetailPage {postId}</div>;
+  useEffect(() => {
+    postDetailApiCall();
+  }, []);
+
+  const postDetailData = useMemo(() => {
+    const channelName = postDetailState?.channel?.name || '';
+    const titleBody = postDetailState?.title || '';
+    let title = '';
+    let body = '';
+    if (titleBody.length > 0) {
+      title = JSON.parse(titleBody)['title'];
+      body = JSON.parse(titleBody)['body'];
+    }
+
+    return {
+      channelName,
+      title,
+      body,
+    };
+  }, [postDetailState]);
+  const { channelName, title, body } = postDetailData;
+
+  return (
+    <PostDetailContainer>
+      <Header isLogo={false} isSearch={false} title={channelName}></Header>
+      <PostDetailBody>
+        <Title>{title}</Title>
+        <Body>{body}</Body>
+      </PostDetailBody>
+    </PostDetailContainer>
+  );
 };
 
 export default PostDetailPage;
