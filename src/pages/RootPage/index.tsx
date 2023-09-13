1e25ccfcd5fe730a71abed40e4bd839dbadf8aab
seeyoujeong <chan3358@naver.com>
2023-09-13 17:11:52 +0900

    feat: #103 pathname에 따른 Header의 조건부 렌더링 추가

--- src/pages/RootPage/index.tsx
+++ src/pages/RootPage/index.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation, useParams } from 'react-router-dom';
 import { getAuthorizationCheckApi } from '~/api/authorization';
+import { getUser } from '~/api/user';
 import { User } from '~/types';
 import Footer from './Footer';
 import Header from './Header';
 
 const RootPage = () => {
   const [userId, setUserId] = useState('');
+  const [username, setUsername] = useState('');
+  const { pathname } = useLocation();
+  const params = useParams();
 
   useEffect(() => {
-    const getUser = async () => {
+    const getMyUserId = async () => {
       const user = (await getAuthorizationCheckApi()) as unknown as
         | User
         | false;
@@ -17,12 +21,34 @@ const RootPage = () => {
       if (user) setUserId(user._id);
     };
 
-    getUser();
+    getMyUserId();
+  });
+
+  useEffect(() => {
+    const getUsername = async (userId: string | undefined) => {
+      if (!userId) return;
+
+      const user = (await getUser(userId)) as unknown as
+        | (User & { username: string })
+        | false;
+
+      if (user) setUsername(user.username);
+    };
+
+    getUsername(params.userId);
   });
 
   return (
     <div>
-      <Header title="" isLogo={true} isSearch={true} />
+      {['/', '/post'].includes(pathname) && <Header isSearch={true} />}
+      {pathname === '/notification' && <Header />}
+      {pathname.includes('/user') &&
+        params.userId &&
+        (params.userId !== userId ? (
+          <Header title={username} isLogo={false} />
+        ) : (
+          <Header isLogout={true} />
+        ))}
       <div style={{ height: '51px' }}></div>
       <Outlet />
       <Footer userId={userId} />
