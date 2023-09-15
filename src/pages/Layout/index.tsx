13bf4cd402a3334afa893ec2ed335158b16e6796
seeyoujeong <chan3358@naver.com>
2023-09-15 16:50:29 +0900

    refactor: #138 Layout에서 Header 컴포넌트 제거

--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,16 +1,11 @@
 import { useEffect, useState } from 'react';
-import { Outlet, useLocation, useParams } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { getAuthorizationCheckApi } from '~/api/authorization';
-import { getUser } from '~/api/user';
 import { User } from '~/types';
 import Footer from './Footer';
-import Header from './Header';
 
 const RootPage = () => {
   const [userId, setUserId] = useState('');
-  const [username, setUsername] = useState('');
-  const { pathname } = useLocation();
-  const params = useParams();
 
   useEffect(() => {
     const getMyUserId = async () => {
@@ -24,32 +19,8 @@ const RootPage = () => {
     getMyUserId();
   });
 
-  useEffect(() => {
-    const getUsername = async (userId: string | undefined) => {
-      if (!userId) return;
-
-      const user = (await getUser(userId)) as unknown as
-        | (User & { username: string })
-        | false;
-
-      if (user) setUsername(user.username);
-    };
-
-    getUsername(params.userId);
-  });
-
   return (
     <div>
-      {['/', '/post'].includes(pathname) && <Header isSearch={true} />}
-      {pathname === '/notification' && <Header />}
-      {pathname.includes('/user') &&
-        params.userId &&
-        (params.userId !== userId ? (
-          <Header title={username} isLogo={false} />
-        ) : (
-          <Header isLogout={true} />
-        ))}
-      <div style={{ height: '51px' }}></div>
       <Outlet />
       <Footer userId={userId} />
     </div>
