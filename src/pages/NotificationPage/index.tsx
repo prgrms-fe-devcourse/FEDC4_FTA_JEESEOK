00d77ecbec531844b60292bbcbcc6f7abc109b45
1eecan <busyx2modernsociety@gmail.com>
2023-09-18 16:38:50 +0900

    refactor: #150_알림페이지 리팩터링,알림 전체 삭제 기능 추가,Like 타입수정

--- src/pages/NotificationPage/index.tsx
+++ src/pages/NotificationPage/index.tsx
@@ -1,111 +1,49 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { getAuthorizationCheckApi } from '~/api/authorization';
+import { readNotification } from '~/api/notification';
 import { Notification } from '~/types';
-
-// export interface Notification {
-//   seen: boolean;
-//   _id: string;
-//   author: User;
-//   user: User | string;
-//   post: string | null;
-//   follow?: string;
-//   comment?: Comment;
-//   message?: string;
-//   createdAt: string;
-//   updatedAt: string;
-// }
+import NotificationCardList from './NotificationCardList';
 
 const NotificationPageWrapper = styled.div`
-width;100%
-`;
+  width;100%
+  `;
 
 const NotificationPage = () => {
   const navigate = useNavigate();
+  const [notifications, setNotifications] = useState<Notification[]>([]);
+
+  const getAuthCheck = async () => {
+    const isAuth = await getAuthorizationCheckApi();
+    if (!isAuth) {
+      navigate('/');
+      return;
+    }
+    setNotifications(isAuth.notifications);
+  };
 
   useEffect(() => {
-    const getAuthCheck = async () => {
-      const isAuth = await getAuthorizationCheckApi();
-      console.log(isAuth);
-      //만약 error나면 어떻게 처리할거임
-      if (typeof isAuth === 'string') {
-        navigate('/');
-      }
-    };
     getAuthCheck();
   }, [navigate]);
 
+  const handleAllNotificationDelete = async () => {
+    if (confirm('알림을 전체 삭제하시겠습니까?')) {
+      await readNotification();
+      await getAuthCheck();
+    }
+  };
+
   return (
     <NotificationPageWrapper>
-      {dummy_notification.map((notification) => (
-        <div>
-          <span>{notification.comment.comment}</span>
-          <span>{notification.createdAt}</span>
-        </div>
-      ))}
+      <button onClick={handleAllNotificationDelete}>알림전체 삭제</button>
+      {notifications.length > 0 ? (
+        <NotificationCardList {...notifications} />
+      ) : (
+        <div>알림이없습니다</div>
+      )}
     </NotificationPageWrapper>
   );
 };
 
 export default NotificationPage;
-
-const dummy_notification = [
-  {
-    seen: false,
-    _id: 'id1',
-    author: 'User',
-    user: 'User | string',
-    post: 'string | null',
-    follow: 'string',
-    comment: {
-      _id: '보낸아이디',
-      comment: '댓글1',
-      author: 'User',
-      post: 'String',
-      createdAt: 'String',
-      updatedAt: 'String',
-    },
-    message: 'string',
-    createdAt: '방금1',
-    updatedAt: '방금1',
-  },
-  {
-    seen: false,
-    _id: 'id2',
-    author: 'User',
-    user: 'User | string',
-    post: 'string | null',
-    follow: 'string',
-    comment: {
-      _id: '보낸아이디2',
-      comment: '댓글2',
-      author: 'User',
-      post: 'String',
-      createdAt: 'String',
-      updatedAt: 'String',
-    },
-    message: 'string',
-    createdAt: '방금2',
-    updatedAt: '방금2',
-  },
-  {
-    seen: false,
-    _id: 'id3',
-    author: 'User',
-    user: 'User | string',
-    post: 'string | null',
-    follow: 'string',
-    comment: {
-      _id: '보낸아이디3',
-      comment: '댓글3',
-      author: 'User',
-      post: 'String',
-      createdAt: 'String',
-      updatedAt: 'String',
-    },
-    message: 'string',
-    createdAt: '방금3',
-    updatedAt: '방금3',
-  },
-];
