87d35d16ff7f3e2d5e94c7059468f8c292b26382
seeyoujeong <chan3358@naver.com>
2023-09-19 18:37:46 +0900

    refactor: #184 assets 폴더 레이어 묶기

---
+++ src/pages/Layout/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as createImg } from './create.svg';
+export { default as loginImg } from './login.svg';
+export { default as logoImg } from './logo.svg';
+export { default as myInfoImg } from './myInfo.svg';
+export { default as notificationImg } from './notification.svg';
+export { default as signupImg } from './signup.svg';
