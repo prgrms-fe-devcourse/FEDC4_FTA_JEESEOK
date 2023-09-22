3f8e24e9f20ecd84198eadfa2d1ee8bd8c6163c0
seeyoujeong <chan3358@naver.com>
2023-09-22 22:02:15 +0900

    feat: #258 댓글 관련 Modal 적용

--- src/components/postDetail/CommentCard/index.tsx
+++ src/components/postDetail/CommentCard/index.tsx
@@ -1,5 +1,7 @@
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import defaultProfile from '~/assets/default_profile.svg';
+import Modal from '~/components/common/Modal';
 import { Comment } from '~/types';
 import {
   Cancel,
@@ -39,6 +41,12 @@ const CommentCard = ({
   const moveUserPage = () => {
     navigate(`/user/${author?._id}`);
   };
+  const [modalState, setModalState] = useState(false);
+
+  const handleConfirmButtonClick = () => {
+    onDeleteComment(_id);
+    setModalState(false);
+  };
 
   return (
     <CommentContainer>
@@ -56,9 +64,17 @@ const CommentCard = ({
       </CommentTopContainer>
       <CommentMiddleContainer>
         <CommentContent>{comment}</CommentContent>
-        {isLogin && <Cancel onClick={() => onDeleteComment(_id)}>삭제</Cancel>}
+        {isLogin && <Cancel onClick={() => setModalState(true)}>삭제</Cancel>}
       </CommentMiddleContainer>
       <Underline />
+      {modalState && (
+        <Modal
+          handleConfirmButtonClick={handleConfirmButtonClick}
+          handleCloseButtonClick={() => setModalState(false)}
+        >
+          댓글을 정말 삭제하시겠습니까?
+        </Modal>
+      )}
     </CommentContainer>
   );
 };
