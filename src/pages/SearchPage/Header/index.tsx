f45571e4f48768ec36da22258b5cee55b9d0255f
seeyoujeong <chan3358@naver.com>
2023-09-22 16:35:44 +0900

    refactor: #253 Header의 로직과 스타일 분리

--- src/pages/SearchPage/Header/index.tsx
+++ src/pages/SearchPage/Header/index.tsx
@@ -1,5 +1,11 @@
 import { useNavigate } from 'react-router-dom';
-import styled from '@emotion/styled';
+import {
+  BackButtonStyled,
+  CloseButtonStyled,
+  HeaderWrapper,
+  Input,
+  InputBox,
+} from './HeaderStyle';
 import backImg from './back.svg';
 import cancelImga from './cancel.svg';
 
@@ -9,57 +15,6 @@ interface HeaderProps {
   setPostArr: (e: []) => void;
   setUserArr: (e: []) => void;
 }
-
-const HeaderWrapper = styled.div`
-  width: 100%;
-  height: 60px;
-  background-color: #f5f9ff;
-  border-radius: 0 0 50px 50px;
-  display: flex;
-  align-items: center;
-  flex-shrink: 0;
-`;
-
-const InputBox = styled.div`
-  display: flex;
-  align-items: center;
-  width: 80%;
-  height: 50%;
-  box-sizing: border-box;
-  border-radius: 30px;
-  margin-right: 10px;
-  margin-left: 10px;
-  background-color: #d9e4fb;
-`;
-
-const Input = styled.input`
-  background-color: transparent;
-  width: 90%;
-  height: 100%;
-  border: none;
-  outline: none;
-  font-size: 15px;
-  padding: 5px 15px;
-  color: #8b9dc6;
-  font-family: 'GangwonEdu_OTFBoldA';
-`;
-
-const BackButtonStyled = styled.button`
-  border: none;
-  background-color: transparent;
-  margin-left: 30px;
-  margin-top: 3px;
-  padding: 0;
-  cursor: pointer;
-`;
-
-const CloseButtonStyled = styled.button`
-  border: none;
-  background-color: transparent;
-  margin-right: 15px;
-  padding: 0;
-  cursor: pointer;
-`;
 
 const Header = ({ word, setWord, setPostArr, setUserArr }: HeaderProps) => {
   const navigate = useNavigate();
